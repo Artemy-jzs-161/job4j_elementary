0a4df8015b1678c89f6faeeb7b42b958007ba8d8
user <borovik.artemy@yandex.ru>
2023-11-28 22:01:17 +0700

    refactor test class

--- src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
+++ src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -1,27 +1,56 @@
 package ru.job4j.loop;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class CheckPrimeNumberTest {
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CheckPrimeNumberTest {
     @Test
-    public void when5() {
+    void when5ThenTrue() {
         int number = 5;
         boolean result = CheckPrimeNumber.check(number);
-        Assert.assertTrue(result);
+        assertThat(result).isTrue();
     }
 
     @Test
-    public void when4() {
+    void when4ThenFalse() {
         int number = 4;
         boolean result = CheckPrimeNumber.check(number);
-        Assert.assertFalse(result);
+        assertThat(result).isFalse();
     }
 
     @Test
-    public void when1() {
+    void when1ThenFalse() {
         int number = 1;
         boolean result = CheckPrimeNumber.check(number);
-        Assert.assertFalse(result);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void when11ThenTrue() {
+        int number = 11;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void when25ThenFalse() {
+        int number = 25;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void when9ThenFalse() {
+        int number = 9;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void when49ThenFalse() {
+        int number = 49;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
     }
 }
